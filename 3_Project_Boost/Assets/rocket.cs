7057409a3972a6185d21228d494b241fd52f8d0b
Meruman <36783697+Meruman@users.noreply.github.com>
2018-08-05 19:41:50 -0500

    Rotate and space keys working

---
+++ 3_Project_Boost/Assets/rocket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) //Thruss and rotate at the same time
+        {
+            print("Space pressed");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("rotate left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotate right");
+        }
+    }
+}
