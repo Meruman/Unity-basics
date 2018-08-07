b80cd78011731e6cec701808f3d6c7d589fdfd0c
Meruman <36783697+Meruman@users.noreply.github.com>
2018-08-07 00:30:28 -0500

    Audio Source

--- 3_Project_Boost/Assets/rocket.cs
+++ 3_Project_Boost/Assets/rocket.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 
 public class rocket : MonoBehaviour {
     Rigidbody rigidBody;
+    AudioSource RocketThrust;
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        RocketThrust = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         ProcessInput();
-	}
+    }
 
     private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space)) //Thruss and rotate at the same time
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!RocketThrust.isPlaying)
+            {
+                RocketThrust.Play();
+            }
+        }
+        else
+        {
+            RocketThrust.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
