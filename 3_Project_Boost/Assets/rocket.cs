5602835b4fe588e1b15831bd62b6a8f36c63a5a9
Meruman <36783697+Meruman@users.noreply.github.com>
2018-08-19 13:39:18 -0500

    Serialize Thrust speeds

--- 3_Project_Boost/Assets/rocket.cs
+++ 3_Project_Boost/Assets/rocket.cs
@@ -4,16 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class rocket : MonoBehaviour {
+    [SerializeField] float rcsThrust = 150f;
+    [SerializeField] float mainThrust = 100f;
     Rigidbody rigidBody;
     AudioSource RocketThrust;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
         rigidBody = GetComponent<Rigidbody>();
         RocketThrust = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
         Thrust();
         Rotate();
     }
@@ -21,7 +25,7 @@ public class rocket : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Space)) //Thruss and rotate at the same time
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!RocketThrust.isPlaying)
             {
                 RocketThrust.Play();
@@ -35,14 +39,16 @@ public class rocket : MonoBehaviour {
     private void Rotate()
     {
         rigidBody.freezeRotation = true; //Take manual control of rotation
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
-        rigidBody.freezeRotation = false; //Resume Physics
+       // rigidBody.freezeRotation = false; //Resume Physics
     }
 }
